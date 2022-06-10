ee4b37c88c7bfe5561eb2cc48d567a1799a66a51
Fernando <luisferomeg@gmail.com>
2022-06-10 15:42:05 -0500

    elimine archivos iniciales que queria que fueran toda una pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 
 @NgModule({
   declarations: [
     AppComponent
-=======
-import { InicioComponent } from './inicio/inicio.component';
-import { LenguajesComponent } from './lenguajes/lenguajes.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { HistoryComponent } from './history/history.component';
-import { ProyectsComponent } from './proyects/proyects.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    InicioComponent,
-    LenguajesComponent,
-    AboutMeComponent,
-    HistoryComponent,
-    ProyectsComponent
->>>>>>> 2ec9cb339acbc89b47d90183b3bbea788a7707d1
   ],
   imports: [
     BrowserModule,
